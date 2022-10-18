f8e67aab6db82c3e3002340461291a1a45ccf215
Ivan Shubin <ivan.ishubin@gmail.com>
2022-10-18 21:43:17 +0200

    updated docs and added static exporter script

---
+++ static-export.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+url="http://localhost:4010"
+
+curl -s -X POST "$url/v1/static-export/start"
+
+
+echo ""
+echo "Waiting for static exporter to finish..."
+attemptsLeft=11
+
+until curl -s "$url/v1/static-export/status" | grep '"finished"'
+do
+    attemptsLeft=$((attemptsLeft-1))
+    if [[ "$attemptsLeft" == "0" ]]; then
+        echo "Timeout waiting for static exporter to finish"
+        exit 1
+    fi
+    sleep 1
+done
+
+echo "Documents are exported"
